5aacc9ef38803b362fe9d5da2a62adedcb6f3635
ibonplix <55146998+ibonplix@users.noreply.github.com>
2020-02-03 13:39:55 +0100

    Add files via upload

---
+++ scriptingadso/6.2 ps o batch/6-2-1.ps1
@@ -0,0 +1,17 @@
+﻿[int]$num1= Read-Host 'Dime el primer numero'
+[int]$num2= Read-Host 'Dime el segundo numero'
+$numvar=0
+if($num1 -lt $num2) {
+   $numvar=$num1
+   $num1=$num2
+   $num2=$numvar
+}
+$suma=$num1+$num2
+$rest=$num1-$num2
+$div=$num1/$num2
+$mult=$num1*$num2
+echo "La suma de $num1 + $num2 = $suma"
+echo "La resta de $num1 - $num2 = $rest"
+echo "La divison de $num1 / $num2 = $div"
+echo "La multiplicacion de $num1 * $num2 = $mult"
+
---
+++ scriptingadso/6.2 ps o batch/6-2-2.ps1
@@ -0,0 +1,23 @@
+﻿[int]$num1= Read-Host 'Dime el primer numero'
+[int]$num2= Read-Host 'Dime el segundo numero'
+$numvar=0
+if($num1 -lt $num2) {
+   $numvar=$num1
+   $num1=$num2
+   $num2=$numvar
+}
+$suma=$num1+$num2
+$rest=$num1-$num2
+$div=$num1/$num2
+$mult=$num1*$num2
+
+echo "La suma de $num1 + $num2 = $suma"
+echo "La resta de $num1 - $num2 = $rest"
+if($num2 -eq 0) {
+    echo "No puede realizarse una division por 0"
+}else{
+    echo "La divison de $num1 / $num2 = $div"
+}
+
+
+echo "La multiplicacion de $num1 * $num2 = $mult"
